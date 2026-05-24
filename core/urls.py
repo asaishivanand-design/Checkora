e39d0ceeb8784530c72fff6ee5158a6ac9926bcf
Richa Chauhan <richachauhan0315@gmail.com>
2026-05-24 18:56:37 +0530

    Improve password recovery flow for shared-email accounts

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,8 @@ from django.contrib.auth import views as auth_views
 from game.views import CustomPasswordResetView
 from django.views.generic import TemplateView
 from django.shortcuts import render
-from game.forms import (
-    CustomPasswordResetForm,
-    CustomSetPasswordForm
-)
+from game.views import password_reset_account_selection
+from game.forms import CustomSetPasswordForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
